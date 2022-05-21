b1311da018d48170a04eff9573699d9d9f920a54
ParvathiNagaraj <103828406+ParvathiNagaraj@users.noreply.github.com>
2022-05-21 16:52:55 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,185 @@
+package week6.day1;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment2 {
+
+	@Test(priority = -1)
+	public void runCreateLead() {
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+		System.out.println("create");
+		driver.close();
+
+	}
+
+	@Ignore
+	@Test
+	//@Test(enabled = false)
+
+	public void runDeleteLead() throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phone']")).click();
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("9");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(2000);
+		String leadID = driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a"))
+				.getText();
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.findElement(By.linkText("Delete")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//input[@name='id']")).sendKeys(leadID);
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		String text = driver.findElement(By.className("x-paging-info")).getText();
+		if (text.equals("No records to display")) {
+			System.out.println("Text matched");
+		} else {
+			System.out.println("Text not matched");
+		}
+		System.out.println("delete");
+		driver.close();
+
+	}
+
+	@Test(invocationCount = 5, threadPoolSize = 2)
+	public void runDuplicateLead() throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phone']")).click();
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("99");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.findElement(By.linkText("Duplicate Lead")).click();
+		driver.findElement(By.name("submitButton")).click();
+		System.out.println("duplicate");
+		driver.close();
+
+	}
+
+	@Test(priority = 0)
+	public void runEditLead() throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phone']")).click();
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("99");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys("TCS");
+		driver.findElement(By.name("submitButton")).click();
+		System.out.println("edit");
+		driver.close();
+
+	}
+
+	@Test(invocationCount = 2, invocationTimeOut = 5000)
+
+	public void runMergeLead() throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Merge Leads")).click();
+		driver.findElement(By.xpath("//img[@alt='Lookup']")).click();
+		Set<String> allWindows = driver.getWindowHandles();
+		List<String> allhandles = new ArrayList<String>(allWindows);
+		driver.switchTo().window(allhandles.get(1));
+		driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("Hari");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(1000);
+		String leadID = driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a"))
+				.getText();
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.switchTo().window(allhandles.get(0));
+
+		driver.findElement(By.xpath("(//img[@alt='Lookup'])[2]")).click();
+		Set<String> allWindows2 = driver.getWindowHandles();
+		List<String> allhandles2 = new ArrayList<String>(allWindows2);
+		driver.switchTo().window(allhandles2.get(1));
+		driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("R");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.switchTo().window(allhandles2.get(0));
+		driver.findElement(By.xpath("//a[text()='Merge']")).click();
+		driver.switchTo().alert().accept();
+
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//input[@name='id']")).sendKeys(leadID);
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		String text = driver.findElement(By.className("x-paging-info")).getText();
+		if (text.equals("No records to display")) {
+			System.out.println("Text matched");
+		} else {
+			System.out.println("Text not matched");
+		}
+		System.out.println("merge");
+
+		driver.close();
+	}
+
+}
